8087756efc82f041d180496356a7889d53f73618
JLuisHdz <joseluishdz02@gmail.com>
2026-02-28 14:59:38 -0600

    feat(auth): implement HTTP interceptor for JWT authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
 
@@ -10,7 +11,9 @@ export const appConfig: ApplicationConfig = {
     
     provideRouter(routes),
 
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   
   ]
 };
