907516670240457ebeadef5cec073773364fae8d
JLuisHdz <joseluishdz02@gmail.com>
2026-03-09 21:43:00 -0600

    feat(dashboard): add inventory statistics pie chart using ng2-chart

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../core/services/product.service';
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgChartsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
 
@@ -15,14 +16,28 @@ export class DashboardComponent {
     ) {}
 
   stats: any;
+  chartData: any;
 
 ngOnInit(): void {
 
   this.productService.getStats().subscribe({
 
     next: res => {
-      console.log(res);
+
       this.stats = res.data;
+
+      this.chartData = {
+        labels: ['Total Products', 'Low Stock'],
+        datasets: [
+          {
+            data: [
+              this.stats.totalProducts,
+              this.stats.lowStockProducts
+            ]
+          }
+        ]
+      };
+
     }
 
   });
