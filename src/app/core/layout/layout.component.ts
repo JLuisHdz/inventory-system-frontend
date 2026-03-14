0b13c26a47b6780753ab0203adea8e5dab41befc
JLuisHdz <joseluishdz02@gmail.com>
2026-03-13 20:58:37 -0600

    style(layout): add lucide icons to sidebar navigation

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, LayoutDashboard, Package } from 'lucide-angular';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LucideAngularModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  readonly DashboardIcon = LayoutDashboard;
+  readonly ProductsIcon = Package;
 
   constructor(
     private authService: AuthService,
